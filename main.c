86f63758a36d274b3024ed5323230ef07c311fa0
Ethan <ethangbraun@gmail.com>
2021-07-03 15:05:14 -0500

    added some info to the settings file

--- main.c
+++ main.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main(){
-    int testing = 8;
-    printf("Yo what's popping\n");
-    return 0;
+    int c, word, lc, state, maxWordLength;
+    int wordArray[10];
+    state = IN;
+    lc = 0;
+    maxWordLength = 0;
+    word = 0;
+
+    for(int i = 0; i < 10; i++)
+        wordArray[i] = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\n' || c ==  ' ' || c == '\t') {
+            if (state == IN){
+                wordArray[word] = lc;
+                if(lc > maxWordLength)
+                    maxWordLength = lc;
+                word++;
+                lc = 0;
+                state = OUT;
+            }
+        } else {
+            state = IN;
+            lc++;
+        }
+    }
+
+    printf("\n");
+
+    for(int i = 0; i < maxWordLength; i++){
+        word = 0;
+        while (wordArray[word] != 0){
+            if (wordArray[word] > i)
+                printf("|");
+            else
+                printf(" ");
+
+            printf(" ");
+            word++;
+        }
+        printf("\n");
+    }
+
 }
