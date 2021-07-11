317bf1df74100a1682defafc9c25368e2045c8ad
Ethan <ethangbraun@gmail.com>
2021-07-11 15:38:54 -0500

    Starting to learn and work on pointers. Should be fun.

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include "testFunctions.c"
 #include "expand.c"
 
-int main(){
-    double atof(char s[]);
-    double test = atof("12.6e1e2");
 
-    printf("%.9f\n", test);
+int main(){
+    int x = 9;
+    int y = 10;
+    return 0;
 }
